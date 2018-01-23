d22e0311c0455321f459a310cdfefd362496fe4d
Lars Wilhelmsen <lars@sral.org>
2018-01-23 14:33:41 +0100

    Add project files.

---
+++ HarMorten/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HarMorten.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
