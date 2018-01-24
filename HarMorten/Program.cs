2d5952b029bd9e8cea750c896497d0bb9aa991c1
Lars Wilhelmsen <lars@sral.org>
2018-01-24 10:04:42 +0100

    Added ApplicationInsights

--- HarMorten/Program.cs
+++ HarMorten/Program.cs
@@ -12,6 +12,7 @@ namespace HarMorten
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
